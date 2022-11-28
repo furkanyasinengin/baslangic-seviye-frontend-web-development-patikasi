974616f2daeb3c0718b415cbd6380de58ff91521
Furkan Yasin Engin <furkanyasinengin@gmail.com>
2022-11-28 19:51:40 +0300

    remove list element function added

--- js/odev2/js/script.js
+++ js/odev2/js/script.js
@@ -1,24 +1,115 @@
 const listDOM = document.querySelector("#list");
 const inputTaskDOM = document.querySelector("#task");
-const listElements = ["3 Litre Su İç", "Ödevleri Yap", "En Az 3 Saat Kodlama Yap"
-  , "Yemek Yap", "50 Sayfa Kitap Oku"]
-listElements.forEach(item => {
-  let liDOM = document.createElement("li");
-  liDOM.onclick = clickListElement
-  liDOM.innerHTML = item;
-  listDOM.append(liDOM);
-})
+// varsayılan liste
+const defaultlistElements = [
+  {
+    name: "3 Litre Su İç",
+    isChecked: false,
+  },
+  {
+    name: "Ödevleri Yap",
+    isChecked: false,
+  },
+  {
+    name: "En Az 3 Saat Kodlama Yap",
+    isChecked: false,
+  },
+  {
+    name: "Yemek Yap",
+    isChecked: false,
+  },
+  {
+    name: "50 Sayfa Kitap Oku",
+    isChecked: false,
+  },
+];
+//localStroge'dan kayıtlı liste kontrolü ve atama işlemi
+localStorage.getItem("listElements")
+  ? (listElements = JSON.parse(localStorage.getItem("listElements")))
+  : (listElements = defaultlistElements);
 
+// liste elemanları için silme butonu
+const closeButton = () => {
+  let buttonDOM = document.createElement("button");
+  let spanDOM = document.createElement("span");
+
+  spanDOM.innerHTML = "&times;";
+
+  buttonDOM.addEventListener("click", closeListElement);
+  buttonDOM.type = "button";
+  buttonDOM.classList.add("close");
+  buttonDOM.append(spanDOM);
+
+  return buttonDOM;
+};
+
+// liste elemanlarının liste elemanlarına atanması
+listElements.forEach((item) => {
+  let liDOM = document.createElement("li");
+  liDOM.addEventListener("click", clickListElement);
+  liDOM.innerHTML = item.name;
+
+  item.isChecked ? liDOM.classList.add("checked") : {};
+
+  liDOM.append(closeButton());
+  listDOM.append(liDOM);
+});
+
+//yeni eleman eklenmesi
 function newElement() {
   let liDOM = document.createElement("li");
-  liDOM.onclick = clickListElement
+  liDOM.addEventListener("click", clickListElement);
   if (inputTaskDOM.value.trim()) {
     liDOM.innerHTML = inputTaskDOM.value;
-    listElements.push(inputTaskDOM.value)
+    liDOM.append(closeButton());
+    listElements.push({ name: inputTaskDOM.value, isChecked: false });
     listDOM.append(liDOM);
+    inputTaskDOM.value = "";
+    localStorage.setItem("listElements", JSON.stringify(listElements));
   }
 }
 
+// liste elemanlarına tıklandığına liste elemanına class eklenmesi
 function clickListElement() {
-  console.log(this.classList.value ? this.classList.remove("checked") : this.classList.add("checked"))
+  try {
+    if (this.classList.value) {
+      this.classList.remove("checked");
+
+      listElements.find(
+        (x) =>
+          x.name === this.textContent.substring(0, this.textContent.length - 1)
+      ).isChecked = false;
+      localStorage.setItem("listElements", JSON.stringify(listElements));
+    } else {
+      this.classList.add("checked");
+      listElements.find(
+        (x) =>
+          x.name === this.textContent.substring(0, this.textContent.length - 1)
+      ).isChecked = true;
+      localStorage.setItem("listElements", JSON.stringify(listElements));
+    }
+  } catch (err) {
+    {
+    }
+  }
+}
+
+// yapılan işin listeden kaldırılması
+function closeListElement() {
+  this.parentElement.remove();
+
+  listElements.splice(
+    listElements.indexOf(
+      listElements.find(
+        (x) =>
+          x.name ===
+          this.parentElement.textContent.substring(
+            0,
+            this.parentElement.textContent.length - 1
+          )
+      )
+    ),
+    1
+  );
+  localStorage.setItem("listElements", JSON.stringify(listElements));
 }
